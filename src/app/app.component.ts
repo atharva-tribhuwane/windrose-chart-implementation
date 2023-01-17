ab5b476849d810102879ca9d60771dad02d20465
Atharva Tribhuwane <100659665+atharva-tribhuwane@users.noreply.github.com>
2023-01-18 02:19:48 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template:
+    ` <ejs-chart id='chartcontainer' [primaryXAxis]='primaryXAxis' [primaryYAxis]='primaryYAxis'
+            [title]='title' >
+            <e-series-collection>
+                <e-series [dataSource]='data' type='Polar' xName='x' yName='y' drawType='Line'> </e-series>
+                <e-series [dataSource]='data' type='Polar' xName='x' yName='y' drawType='Line'> </e-series>
+                <e-series [dataSource]='data' type='Polar' xName='x' yName='y' drawType='Line'> </e-series>
+                <e-series [dataSource]='data' type='Polar' xName='x' yName='y' drawType='Line'> </e-series>
+                <e-series [dataSource]='data' type='Polar' xName='x' yName='y' drawType='Line'> </e-series>
+                <e-series [dataSource]='data' type='Polar' xName='x' yName='y' drawType='Line'> </e-series>
+            </e-series-collection>
+    </ejs-chart>`
 })
-export class AppComponent {
-  title = 'windrose';
-}
+export class AppComponent implements OnInit {
+    public primaryXAxis: Object | undefined;
+    public title: string | undefined;
+    public primaryYAxis: Object | undefined;
+    public data: Object[] | undefined;
+
+
+    ngOnInit(): void {
+        this.data = [{ x: 'N', y: 28 }, { x: 'E', y: 25 },{ x:'S' , y: 26 }, { x: 'W', y: 27 },{ x: 'NH', y: 27 },{ x: 'BHG', y: 27 }];
+        this.primaryXAxis = {
+            title: 'Direction',
+            valueType: 'Category'
+            };
+        this.primaryYAxis = {
+            minimum: 0, maximum: 10, interval: 2,
+            title: 'Efficiency',
+            };
+
+        this.title = 'Efficiency of oil-fired power production';
+
+    }
+}//https://ej2.syncfusion.com/angular/demos/chart/polar-stacking-column/
+//https://ej2.syncfusion.com/angular/documentation/chart/polar-radar?cs-save-lang=1&cs-lang=ts
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ChartModule, ChartAllModule } from '@syncfusion/ej2-angular-charts';
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { AreaSeriesService, LineSeriesService, ExportService, ColumnSeriesService, StackingColumnSeriesService, StackingAreaSeriesService, RangeColumnSeriesService, ScatterSeriesService, PolarSeriesService, CategoryService, RadarSeriesService, ILoadedEventArgs, SplineSeriesService} from '@syncfusion/ej2-angular-charts';
 
+/**
+ * Module
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule, ChartModule, ButtonModule, ChartAllModule
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    providers: [ AreaSeriesService, LineSeriesService, ExportService, ColumnSeriesService, StackingColumnSeriesService, StackingAreaSeriesService, RangeColumnSeriesService, ScatterSeriesService, PolarSeriesService, CategoryService, RadarSeriesService, SplineSeriesService]
 })
 export class AppModule { }
